805d3a4b2255dc30f754facbe0843aa52f18b300
Le Xue <lxue@salesforce.com>
2024-06-28 04:14:52 +0000

    [ULIP-2] Update files.

---
+++ scripts/test_ulip2_pointbert_objaverse_lvis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+echo "Please provide a *.pt file as input"
+exit 1
+fi
+
+model_file=$1
+output_dir=./outputs/test_pointbert_8kpts
+
+CUDA_VISIBLE_DEVICES=0 python main.py --model ULIP2_PointBERT_Colored --npoints 10000 --output-dir $output_dir --evaluate_3d_ulip2 --validate_dataset_name=objaverse_lvis_colored --test_ckpt_addr $model_file 2>&1 | tee $output_dir/log.txt
